44e3c0b527372077133d13c4138690831603222a
DimaAlameh <dalameh123@gmail.com>
2025-02-13 20:48:57 +0200

    Removed TASK3-Secure_Coding_Review from frontend_development_internship branch

--- TASK3-Secure_Coding_Review/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'  # Set the URI for SQLite database
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disable unnecessary overhead
